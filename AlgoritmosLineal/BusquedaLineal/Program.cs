5790f1d8e4cc61a53aff88f8edad72728db042a6
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2024-03-23 19:44:57 -0600

    Agregar archivos de proyecto.

---
+++ AlgoritmosLineal/BusquedaLineal/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numeros = { 1, 2, 5, 52, 53, 1, 5, 2, 245, 6 };
+        int objeto = 245; // número a buscar
+
+        int indice = BusquedaLineal(numeros, objeto);
+
+        if (indice != -1)
+            Console.WriteLine($"El número {objeto} se encuentra en la posición {indice}");
+        else
+            Console.WriteLine($"El número {objeto} no se encuentra en el arreglo");
+    }
+
+    static int BusquedaLineal(int[] array, int objeto)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] == objeto)
+                return i;
+        }
+        return -1;
+    }
+}
---
+++ AlgoritmosLineal/BusquedaLinealString/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] palabras = { "Hola", "amor", "love", "vida" };
+        string palabra = "love";
+
+        int indice = BusquedaString(palabras, palabra);
+
+        if (indice != -1)
+            Console.WriteLine($"La palabra '{palabra}' se encuentra en la posición {indice}");
+        else
+            Console.WriteLine($"La palabra '{palabra}' no se encuentra");
+    }
+
+    static int BusquedaString(string[] array, string palabra)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] == palabra)
+                return i;
+        }
+        return -1;
+    }
+}
